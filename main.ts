87da169c5ecad433738a96ef3aab2c042e9d6232
kvapehe <33888633+kvapehe@users.noreply.github.com>
2021-11-13 02:30:19 +0100

    Ikke_ Levert

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+input.onButtonPressed(Button.A, function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(1000)
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+    basic.pause(1000)
+})
+input.onButtonPressed(Button.B, function () {
+    range = strip.range(0, 1)
+    strip.showColor(neopixel.colors(NeoPixelColors.Violet))
+    strip.show()
+})
+let range: neopixel.Strip = null
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P1, 14, NeoPixelMode.RGB)
+let verdi = 70
+strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+basic.pause(1000)
+basic.forever(function () {
+	
+})
